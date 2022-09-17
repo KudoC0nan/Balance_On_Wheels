5fad15c318f681a0eb009baf03ae0d3ee2d21f85
Arsh Khan <khanarsh0124@gmail.com>
2022-09-17 18:24:40 +0530

    Added Task 3

--- Task/Task3/main/servo.c
+++ Task/Task3/main/servo.c
@@ -42,21 +42,6 @@ servo_config servo_d = {
 	.gen = MCPWM_OPR_B,
 };
 
-#ifdef CONFIG_ENABLE_OLED
-// Declaring the required OLED struct
-u8g2_t oled_config;
-
-void update_oled()
-{
-	// Diplaying Servo A, Servo B, Servo C, Servo D values on OLED
-	while (1)
-	{
-		display_servo_values(read_servo(&servo_a), read_servo(&servo_b), read_servo(&servo_c), read_servo(&servo_d), &oled_config);
-	}
-}
-
-#endif
-
 static void mcpwm_servo_control(void *arg)
 {
 	enable_servo();
@@ -85,14 +70,5 @@ static void mcpwm_servo_control(void *arg)
 
 void app_main()
 {
-	ESP_LOGD(TAG, "Testing servo motors\n");
-
-#ifdef CONFIG_ENABLE_OLED
-	ESP_ERROR_CHECK(init_oled(&oled_config));
-	display_mario_logo(&oled_config);
-	vTaskDelay(100);
-	xTaskCreatePinnedToCore(update_oled, "update oled", 4096, NULL, 5, NULL, 0);
-#endif
-
 	xTaskCreatePinnedToCore(mcpwm_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL, 1);
 }
